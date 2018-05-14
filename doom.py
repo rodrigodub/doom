64243bfd64e0a4d339f5c44a8cba498b8297e08f
Rodrigo <nobrega@gmail.com>
2018-05-14 16:18:02 +0800

    Setup project
    # v0.01
    # for ticket #1

---
+++ doom.py
@@ -0,0 +1,36 @@
+#################################################
+# Doom
+# Script to adjust read an RFE email
+#
+# v0.01
+# for ticket #1
+#
+# Rodrigo Nobrega
+# 20180514-
+#################################################
+
+# import modules
+import datetime
+import os
+
+
+# define Timeshift class
+class ReadRFE(object):
+	"""
+	Reads contents of an RFE Outlook message file
+	"""
+	pass
+
+
+# main loop
+def main():
+	print('\n=============================================================================')
+	print('                                    Doom')
+	print('                     Reads and processes contents of RFE')
+	print('=============================================================================\n')
+
+
+# main, calling main loop
+if __name__ == '__main__':
+	# test()
+    main()
