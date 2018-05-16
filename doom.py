1e05834bda1cf218d216d443b5a1d53ce2e0c135
Rodrigo <nobrega@gmail.com>
2018-05-16 17:34:33 +0800

    Looking pretty
    # v0.06
    # # for ticket #1

--- doom.py
+++ doom.py
@@ -16,6 +16,16 @@
 
 # global variables
 DIRECTORY = './examples/'
+OUTPUTRFE = 'Customer Name:\n{}\n\n' \
+            'Site:\n\n\n' \
+            'Who has identified the requirement?:\n\n\n' \
+            'Role:\n\n\n' \
+            'acQuire RFE Owner:\n\n\n' \
+            'Original SI#:\n\n\n' \
+            'GIM Suite Version:\n\n\n' \
+            'Description:\n\n\n' \
+            'Workaround:\n\n\n' \
+            'Additional Comments:\n\n\n'
 
 
 # define Timeshift class
@@ -25,20 +35,19 @@ class ReadRFE(object):
     """
     def __init__(self, file):
         self.contents = self.readcontents(file)
+        self.customer = self.outputcustomer()
+
 
     def readcontents(self, file):
         a = ''
-        b = open(r'{}{}'.format(DIRECTORY, file), 'r', encoding = 'ISO-8859-1')
-        # b = open(r'{}{}'.format(DIRECTORY, file), 'r', encoding='ANSI')
+        b = open(r'{}{}'.format(DIRECTORY, file), 'r', encoding='ISO-8859-1')
         for i in b:
-            a = a + r'{}\n'.format(i)
+            a = a + '{}'.format(i.replace('\x00', ''))
         b.close()
         return a
 
     def outputcustomer(self):
-        for i in self.contents:
-            if 'Customer Name' in i:
-                print(i)
+        return self.contents.split('Customer Name')[1].split('Site')[0].replace('\n', '').strip()
 
 
 # main loop
@@ -48,8 +57,7 @@ def main():
     print('                     Reads and processes contents of RFE')
     print('=============================================================================\n')
     rfe = ReadRFE('SI-81772.msg')
-    print(rfe.contents)
-    # rfe.outputcustomer()
+    print(OUTPUTRFE.format(rfe.customer))
 
 
 # main, calling main loop
