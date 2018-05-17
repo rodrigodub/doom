98dc322f5f26200078048bffe4a720202985d9a5
Rodrigo <nobrega@gmail.com>
2018-05-17 16:52:30 +0800

    Output string working well
    # v0.08
    # # for ticket #1

--- doom.py
+++ doom.py
@@ -2,11 +2,11 @@
 # Doom
 # Script to adjust read an RFE email
 #
-# v0.07
+# v0.08
 # # for ticket #1
 #
 # Rodrigo Nobrega
-# 20180514-20180516
+# 20180514-20180517
 #################################################
 
 # import modules
@@ -19,13 +19,13 @@ DIRECTORY = './examples/'
 OUTPUTRFE = 'Customer Name:\n{}\n\n' \
             'Site:\n{}\n\n' \
             'Who has identified the requirement?:\n{}\n\n' \
-            'Role:\n\n\n' \
-            'acQuire RFE Owner:\n\n\n' \
-            'Original SI#:\n\n\n' \
-            'GIM Suite Version:\n\n\n' \
-            'Description:\n\n\n' \
-            'Workaround:\n\n\n' \
-            'Additional Comments:\n\n\n'
+            'Role:\n{}\n\n' \
+            'acQuire RFE Owner:\n{}\n\n' \
+            'Original SI#:\n{}\n\n' \
+            'GIM Suite Version:\n{}\n\n' \
+            'Description:\n{}\n{}\n\n' \
+            'Workaround:\n{}\n\n' \
+            'Additional Comments:\n{}\n\n'
 
 
 # define Timeshift class
@@ -37,14 +37,19 @@ class ReadRFE(object):
         self.contents = self.readcontents(file)
         self.customer = self.returnstring('Customer Name', 'Site')
         self.site = self.returnstring('Site', 'Who has identified')
-        self.who = self.returnstring('Who has identified the requirement?', 'Role')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
-        # self.customer = self.returnstring('Customer Name', 'Site')
+        self.user = self.returnstring('Who has identified the requirement?', 'Role')
+        self.role = self.returnstring('Role', 'GIM Suite Version')
+        self.version = self.returnstring('GIM Suite Version', 'Triage Details')
+        self.owner = self.returnstring('acQuire RFE Owner', 'SI#')
+        self.si = self.returnstring('SI#', 'RFE Summary')
+        self.userstory = self.returnstring('User Story', 'Description')
+        self.description = self.returnstring('Description', 'Problem Type')
+        self.workaround = self.returnstring('Workaround(s)\n\n\n\n \n\n\n\n[Implication]', 'Additional Comments')
+        self.additionalcomments = self.returnstring('Additional Comments', 'v4.1')
+        self.impact1 = self.returnstring('Select Impact level (as rated by the customer)\n\n\n\n \n\n\n\n[Implication]', 'Describe the impact\n\n\n\n \n\n\n\n[Implication]')
+        self.impact2 = self.returnstring('Describe the impact\n\n\n\n \n\n\n\n[Implication]', 'Rate the level of impact to acQuire business\n\n\n\n(if applicable)')
+        self.impact3 = self.returnstring('Rate the level of impact to acQuire business\n\n\n\n(if applicable)', 'Can you quantify the impact:')
+        self.impact4 = self.returnstring('Can you quantify the impact:', 'Workaround(s)')
 
     def readcontents(self, file):
         a = ''
@@ -64,10 +69,11 @@ def main():
     print('                                    Doom')
     print('                     Reads and processes contents of RFE')
     print('=============================================================================\n')
-    # rfe = ReadRFE('SI-77948.msg')
-    rfe = ReadRFE('SI-79383.msg')
+    rfe = ReadRFE('SI-77948.msg')
+    # rfe = ReadRFE('SI-79383.msg')
     # rfe = ReadRFE('SI-81772.msg')
-    print(OUTPUTRFE.format(rfe.customer, rfe.site, rfe.who))
+    print(OUTPUTRFE.format(rfe.customer, rfe.site, rfe.user, rfe.role, rfe.owner, rfe.si, rfe.version,
+                           rfe.userstory, rfe.description, rfe.workaround, rfe.additionalcomments))
 
 
 # main, calling main loop
